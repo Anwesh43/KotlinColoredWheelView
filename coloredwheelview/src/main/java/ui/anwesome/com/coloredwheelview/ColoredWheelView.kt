b5270cb450cd520b238643561b2b11fa7d39c37d
Anwesh43 <anweshthecool0@gmail.com>
2018-03-07 01:54:35 +0530

    adding ColoredWheelView class

--- coloredwheelview/src/main/java/ui/anwesome/com/coloredwheelview/ColoredWheelView.kt
+++ coloredwheelview/src/main/java/ui/anwesome/com/coloredwheelview/ColoredWheelView.kt
@@ -3,3 +3,21 @@ package ui.anwesome.com.coloredwheelview
 /**
  * Created by anweshmishra on 07/03/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+val colors : Array<String> = arrayOf("#f44336", "#4CAF50", "#3949AB", "#7B1FA2", "#880E4F", "#64DD17")
+class ColoredWheelView(ctx : Context, var n : Int = 6) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
