ebbe3eadf077c1bdf8083cb65d87c1d4ac99a956
Anwesh43 <anweshthecool0@gmail.com>
2018-03-07 07:26:03 +0530

    adding State class

--- coloredwheelview/src/main/java/ui/anwesome/com/coloredwheelview/ColoredWheelView.kt
+++ coloredwheelview/src/main/java/ui/anwesome/com/coloredwheelview/ColoredWheelView.kt
@@ -7,7 +7,7 @@ import android.view.*
 import android.content.*
 import android.graphics.*
 val colors : Array<String> = arrayOf("#f44336", "#4CAF50", "#3949AB", "#7B1FA2", "#880E4F", "#64DD17")
-class ColoredWheelView(ctx : Context, var n : Int = 6) : View(ctx) {
+class ColoredWheelView(ctx : Context) : View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas : Canvas) {
 
@@ -19,5 +19,28 @@ class ColoredWheelView(ctx : Context, var n : Int = 6) : View(ctx) {
             }
         }
         return true
+    }
+    data class State(var prevScale : Float = 0f, var dir : Float = 0f, var jDir : Int = 1, var j : Int = 0) {
+        val scales : Array<Float> = arrayOf(0f, 0f, 0f)
+        fun update(stopcb : (Float) -> Unit) {
+            scales[j] += jDir * 0.1f
+            if(Math.abs(scales[j] - prevScale) > 1) {
+                scales[j] = prevScale + dir
+                j += jDir
+                if(j == scales.size || j == -1) {
+                    jDir *= -1
+                    j += jDir
+                    prevScale = scales[j]
+                    dir = 0f
+                    stopcb(prevScale)
+                }
+            }
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            if(dir == 0f) {
+                dir = 1 - 2 * prevScale
+                startcb()
+            }
+        }
     }
 }
