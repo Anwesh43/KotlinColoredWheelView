fd476ad66135806a1ade4f84261de7f5cfe82874
Anwesh43 <anweshthecool0@gmail.com>
2018-03-08 07:24:21 +0530

    adding ColoredWheelView in activity

--- app/src/main/java/ui/anwesome/com/kotlincoloredwheelview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincoloredwheelview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlincoloredwheelview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.coloredwheelview.ColoredWheelView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ColoredWheelView.create(this)
     }
 }
